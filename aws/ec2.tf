b8c35f04f0ccbb6d8ef33e12e618583baf5a2969
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-10 10:50:31 -0500

    adding resources to the tf file

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -6,20 +6,56 @@ terraform {
   }
 }
 
+variable "default_tags" {
+  type = map
+  default = {
+    project = "jenkins"
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
-resource "aws_instance" "jenkins_instance" {
+resource "aws_instance" "jenkins_instance_9" {
   ami                    = "ami-0c94855ba95c71c99"
   instance_type          = "t2.micro"
-  key_name               = "ec2-kp"
+  key_name               = aws_key_pair.jenkins_kp.key_name
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.jenkins_profile.name
   tags = {
-    Name = "Jenkins-server"
+    Name    = "Jenkins-server"
+    project = "Jenkins"
   }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ip_address.txt"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/Documents/keys/terraform")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -q -y install docker",
+      "sudo groupadd docker",
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker $USER",
+      "sudo systemctl status docker",
+      "sudo docker pull -q jenkins/jenkins:lts",
+      "sudo docker run -p 8080:8080 -d --name jenkins_server jenkins/jenkins:lts",
+      "sudo docker exec jenkins_server cat /var/jenkins_home/secrets/initialAdminPassword"
+    ]
+  }
+}
+
+resource "aws_key_pair" "jenkins_kp" {
+  key_name   = "jenkins-kp"
+  public_key = file("~/Documents/keys/terraform.pub")
 }
 
 resource "aws_security_group" "jenkins_sg" {
@@ -54,6 +90,7 @@ resource "aws_security_group" "jenkins_sg" {
 
 resource "aws_iam_instance_profile" "jenkins_profile" {
   name = "jenkins-profile"
+  path = "/jenkins/"
   role = aws_iam_role.jenkins_role.name
 }
 
@@ -67,6 +104,21 @@ resource "aws_iam_role_policy" "jenkins_role_policy" {
   name   = "jenkins-policy"
   role   = aws_iam_role.jenkins_role.id
   policy = data.aws_iam_policy_document.jenkins_policy.json
+}
+
+resource "aws_s3_bucket" "jenkins_s3_data" {
+  acl    = "private"
+  bucket = "jenkins-s3-data"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "jenkins_s3_public_access_block" {
+  bucket = aws_s3_bucket.jenkins_s3_data.id
+
+  block_public_acls   = true
+  block_public_policy = true
 }
 
 data "aws_iam_policy_document" "jenkins_assume_role_policy" {
